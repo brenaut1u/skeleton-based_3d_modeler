beeaa2bd89267e43767aff0d6ee0859b765a0123
Katia Brenaut <katia.brenaut9@etu.univ-lorraine.fr>
2024-01-08 16:59:41 +0100

    Deleted more files (switched from nanobind to pybind)

--- binding/lise.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-cmake -S . -B build
-cmake --build build
-cp lise.py build/
-cd build
-python3 lise.py
-
-
--- modeler.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-cmake -S . -B build -G "MinGW Makefiles"
-cmake --build build
-cp modeler.py build/
-cd build
-python3 modeler.py
