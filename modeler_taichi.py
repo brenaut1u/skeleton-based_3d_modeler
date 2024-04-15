45c9b401fa96b0ec56465d98681e0ab3a9e5680d
LiseBabe <114232131+LiseBabe@users.noreply.github.com>
2024-04-15 16:27:27 +0200

    deleted some print

--- modeler_taichi.py
+++ modeler_taichi.py
@@ -95,10 +95,7 @@ while gui.running:
                     selected_id_past = selected_id
                 else : 
                     ray = ((pos[0]-origin[0])**2+(pos[1]-origin[1])**2)**(1/2)*4
-                    print(ray)
                     modeler1.increaseRadius(selected_id_past,-(origin[0]-pos[0])/n*100)
-                print(origin)
-                print(pos)
             
         elif gui.is_pressed('t'):
                 origin = (-1,-1)
