d73540dcd39120a4ba3642a38938afe54da770eb
LiseBabe <114232131+LiseBabe@users.noreply.github.com>
2024-06-05 14:00:33 +0200

    delete a small function not use

--- modeler_taichi.py
+++ modeler_taichi.py
@@ -217,12 +217,3 @@ while gui.running:
     canvas.set_image(pixels)
 
     gui.show()
-
-
-
-def bubble_sort(arr):
-    for i in range(len(arr) - 1, 0, -1):
-        for j in range(i):
-            if arr[j] > arr[j + 1]:
-                arr[j + 1], arr[j] = arr[j], arr[j + 1]
-    return arr
